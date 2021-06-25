59bbec797feaeb08b1dd40b7bde0b7fbfce9909a
Sander Verbrugge <sander@inktweb.nl>
2021-06-25 12:17:19 +0200

    Generate client for each API version including endpoint methods

--- dev/src/Config.php
+++ dev/src/Config.php
@@ -9,6 +9,9 @@ class Config
     public const ROOT_PATH = __DIR__ . '/../../src';
     public const ROOT_NAMESPACE = 'Inktweb\\Bolcom\\RetailerApi';
 
+    public const CLIENTS_PATH = self::ROOT_PATH . '/Clients';
+    public const CLIENTS_NAMESPACE = self::ROOT_NAMESPACE . '\\Clients';
+
     public const ENDPOINTS_PATH = self::ROOT_PATH . '/Endpoints';
     public const ENDPOINTS_NAMESPACE = self::ROOT_NAMESPACE . '\\Endpoints';
 
