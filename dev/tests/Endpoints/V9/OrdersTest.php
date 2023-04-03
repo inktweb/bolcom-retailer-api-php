abe358a95922077ed7728aed61c07b164ededa0c
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2023-04-03 10:34:01 +0200

    Added endpoint tests for Bol.com API v9

---
+++ dev/tests/Endpoints/V9/OrdersTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Inktweb\Bolcom\RetailerApi\Development\Tests\Endpoints\V9;
+
+use Inktweb\Bolcom\RetailerApi\Models\V9\Retailer\ReducedOrders;
+
+/**
+ * @coversDefaultClass \Inktweb\Bolcom\RetailerApi\Endpoints\V9\Retailer\Orders
+ */
+class OrdersTest extends BaseTest
+{
+    /**
+     * @covers ::getOrders
+     */
+    public function testGetOrders(): void
+    {
+        $orders = $this->getClient()
+            ->retailer()
+            ->orders()
+            ->getOrders();
+
+        $this->assertInstanceOf(ReducedOrders::class, $orders);
+    }
+}
